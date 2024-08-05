b37a5f0500c1d043019e0097e90c985119dddb61
ItamarAharony <64987789+ItamarAharony@users.noreply.github.com>
2024-08-05 16:25:31 -0400

    Update train_model.py

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,1 +1,27 @@
+import numpy as np
+import tensorflow as tf
+from data_preprocessing import load_and_preprocess_data
+
+def build_model():
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+        tf.keras.layers.MaxPooling2D((2, 2)),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D((2, 2)),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(10, activation='softmax')
+    ])
+    return model
+
+def train_model():
+    (train_images, train_labels), (test_images, test_labels) = load_and_preprocess_data()
+    model = build_model()
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.fit(train_images, train_labels, epochs=5, batch_size=64, validation_data=(test_images, test_labels))
+    model.save('saved_models/mnist_cnn_model.h5')
+
+if __name__ == "__main__":
+    train_model()
 
